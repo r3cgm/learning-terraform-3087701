bb3b6d3282925017bef1d254d7a36403b1753b2a
George Mann <r3cgm@icloud.com>
2023-06-16 14:58:25 -0700

    Update main.tf
    Trying to fix bug related to private vars

--- main.tf
+++ main.tf
@@ -22,6 +22,7 @@ module "vpc" {
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = []
 
   enable_nat_gateway = true
 
