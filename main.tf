2e064c7220f92ecf8d51ceb57a86c48f14ccad55
George Mann <r3cgm@icloud.com>
2023-06-17 10:43:38 -0700

    no private subnets

--- main.tf
+++ main.tf
@@ -22,7 +22,6 @@ module "blog_vpc" {
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  private_subnets = ["0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0"]
 
   enable_nat_gateway = true
 
